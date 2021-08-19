9f341498d8a502030d9c5fda12d5bf6c5ea3547f
Stephanie Saavedra Ayarde <stephanie.saavedra.ayarde@gmail.com>
2021-08-19 08:11:24 -0400

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import {catchError, retry } from 'rxjs/operators';
+import { Meme } from './meme';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Generador-de-memes';
+  public memes: Meme[] = [];
+
+
+  constructor(private http: HttpClient){}
+
+  load(): void{
+    /*Proceso Asincrono, suscribe: Una vez que tengas la data lo vamos a procesar*/
+    console.log("Me estas presionando");
+
+    this.http.get<any>("https://api.imgflip.com/get_memes").subscribe(response => {
+      this.memes = response.data.memes;
+      console.log("Llegada de datos");
+    });
+  }
 }
